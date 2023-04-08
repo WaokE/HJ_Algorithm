f68afb9343bd2a6f36f1adad52e177f07749aaa5
WaokE <128684924+WaokE@users.noreply.github.com>
2023-04-08 21:10:53 +0900

    Update 팩토리얼.py

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.py
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.py
@@ -1,7 +1,21 @@
-number = int(input())
-result = 1
+# 재귀로 풀어보자
+import sys
 
-for i in range(1,number+1):
-    result = result*i
+input = int(sys.stdin.readline())
 
-print(result)
+def factorial(num):
+    if num > 1:
+        return num*factorial(num-1)
+    else:
+        return 1
+
+print(factorial(input))
+
+# no 재귀 풀이
+# number = int(input())
+# result = 1
+
+# for i in range(1,number+1):
+#     result = result*i
+
+# print(result)
