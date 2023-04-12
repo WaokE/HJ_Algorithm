0e44cf9f2bafa1bd7b1cf49165bd6d748b4ebe36
WaokE <128684924+WaokE@users.noreply.github.com>
2023-04-12 18:03:54 +0900

    Update 일곱 난쟁이.py

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,16 +1,38 @@
+# import sys
+# inputs = [int(sys.stdin.readline())for _ in range(9)]
+
+# def searchDwarf(dwarfs):
+#     for i in range(len(dwarfs)):
+#         for j in range(len(dwarfs)-1):
+#             selected = list(dwarfs)
+#             del selected[i]
+#             del selected[j]
+#             if sum(selected) == 100:
+#                 selected.sort()
+#                 for k in range(len(selected)):
+#                     print(selected[k])
+#                 return    
+                
+# searchDwarf(inputs)
+
+재귀로 풀이
 import sys
 inputs = [int(sys.stdin.readline())for _ in range(9)]
 
-def searchDwarf(dwarfs):
-    for i in range(len(dwarfs)):
-        for j in range(len(dwarfs)-1):
-            selected = list(dwarfs)
-            del selected[i]
-            del selected[j]
-            if sum(selected) == 100:
-                selected.sort()
-                for k in range(len(selected)):
-                    print(selected[k])
-                return    
-                
-searchDwarf(inputs)
+def searchDwarf(choosed:list) :
+    if len(choosed) == 7:
+        if sum(choosed) == 100:
+            choosed.sort()
+            for i in choosed:
+                print(i)
+            exit()
+        return
+    else:
+        left = list(set(inputs) - set(choosed))
+        for i in range(len(left)):
+            choosenOne = left[i]
+            choosed.append(choosenOne)
+            searchDwarf(choosed)
+            choosed.pop()
+
+searchDwarf([])
