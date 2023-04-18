24d9ed31f1cd99affb16113e35bb522501416b3c
WaokE <128684924+WaokE@users.noreply.github.com>
2023-04-18 14:53:21 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 428 ms, Memory: 52036 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+points = [list(map(int, input().split()))for _ in range(N)]
+
+points.sort(key = lambda x: x[1])
+points.sort(key = lambda x: x[0])
+
+for point in points:
+    print(point[0], end = ' ')
+    print(point[1])
