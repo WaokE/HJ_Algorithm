908a39ea00b1821f3e430b0fb7ed56abfd351c1b
WaokE <128684924+WaokE@users.noreply.github.com>
2023-04-18 20:13:13 +0900

    Update 가운데를 말해요.py

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.py
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.py
@@ -6,6 +6,19 @@ N = int(sys.stdin.readline())
 minHeap = []
 maxHeap = []
 
+def checkHeapDifer(min, max):
+    if len(max) - len(min) == 0 or len(max) - len(min) == 1:
+        pass
+    else:
+        if len(max) > len(min):
+            # 요기 음수 처리 안해서 틀렸었음... 개어이없네
+            heapq.heappush(min, -heapq.heappop(max))
+        elif len(max) < len(min):
+            heapq.heappush(max, -heapq.heappop(min))
+
+        
+
+
 for i in range(N):
     num = int(sys.stdin.readline())
     if i == 0:
@@ -14,16 +27,13 @@ for i in range(N):
     else:
         if -maxHeap[0] == num:
             heapq.heappush(maxHeap, -num)
-            if len(maxHeap) > len(minHeap) + 1:
-                heapq.heappush(minHeap, -heapq.heappop(maxHeap))
+            checkHeapDifer(minHeap, maxHeap)
             print(num)
         elif -maxHeap[0] < num:
             heapq.heappush(minHeap, num)
-            if len(minHeap) > len(maxHeap):
-                heapq.heappush(maxHeap, -heapq.heappop(minHeap))
+            checkHeapDifer(minHeap, maxHeap)
             print(-maxHeap[0])
         elif -maxHeap[0] > num:
             heapq.heappush(maxHeap, -num)
-            if len(maxHeap) > len(minHeap) + 1:
-                heapq.heappush(minHeap, -heapq.heappop(maxHeap))
+            checkHeapDifer(minHeap, maxHeap)
             print(-maxHeap[0])
