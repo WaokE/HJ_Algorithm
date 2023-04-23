a92bba47a5e3a7ad15f0e4d8cdece9df04fe514c
WaokE <128684924+WaokE@users.noreply.github.com>
2023-04-23 11:38:48 +0900

    [Gold IV] Title: 이분 그래프, Time: 2512 ms, Memory: 133608 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -1,39 +1,39 @@
-def is_bipartite(graph):
-    n = len(graph)
-    visited = [0] * n  # 방문 여부 체크 배열
-    stack = []
+import sys
+from collections import deque
+input = sys.stdin.readline
 
-    for start in range(n):
-        if visited[start] == 0:
-            stack.append((start, 1))  # 시작 노드와 색깔(1 또는 -1)을 스택에 추가
-
-            while stack:
-                node, color = stack.pop()
-
-                if visited[node] != 0:
-                    if visited[node] != color:
-                        return False
-                    continue
-
-                visited[node] = color
-
-                for neighbor in graph[node]:
-                    stack.append((neighbor, -color))  # 인접한 노드와 현재 노드와 다른 색깔을 스택에 추가
-
+def BG(vertex):
+    global queue
+    queue.append((vertex, 1))
+    while queue:
+        parent, color = queue.popleft()
+        if visited[parent] != 0:
+            if visited[parent] != color:
+                return False
+            continue
+        visited[parent] = color
+        for child in edgesEachNode[parent]:
+            if visited[child] == color:
+                return False
+            queue.append((child, -color))
     return True
-
 
 K = int(input())
 for _ in range(K):
-    V, E = list(map(int, input().split()))
-    inputs = [list(map(int, input().split())) for _ in range(E)]
-    edgesEachNode = [[] for _ in range(V + 1)]
+    V, E = list(map(int,input().split()))
+    inputs = [list(map(int, input().split()))for _ in range(E)]
+    edgesEachNode = [[]for _ in range(V+1)]
 
     for edge in inputs:
         edgesEachNode[edge[0]].append(edge[1])
         edgesEachNode[edge[1]].append(edge[0])
 
-    if is_bipartite(edgesEachNode):
-        print('YES')
-    else:
-        print('NO')
+    visited = [0]*(V+1)
+    queue = deque()
+    result = 'YES'
+    for i in range(1, V + 1):
+        if visited[i] == 0:
+            if not BG(i):
+                result = 'NO'
+    print(result)
+    
