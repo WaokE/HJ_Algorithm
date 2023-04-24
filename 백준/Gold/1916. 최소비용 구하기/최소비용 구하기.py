f2a60053dfc512c865abe6691d36198344b7d7c8
WaokE <128684924+WaokE@users.noreply.github.com>
2023-04-24 21:18:26 +0900

    [Gold V] Title: 최소비용 구하기, Time: 340 ms, Memory: 63652 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -0,0 +1,26 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+M = int(input())
+inputs = [list(map(int, input().split()))for _ in range(M)]
+start, end = list(map(int, input().split()))
+buses = [[]for _ in range(N+1)]
+for bus in inputs:
+    heapq.heappush(buses[bus[0]],((bus[2], bus[1])))
+
+visitList = []
+costs = [float('inf')]*(N+1)
+costs[start] = 0
+heapq.heappush(visitList, (costs[start], start))
+
+while visitList:
+    cost, position = heapq.heappop(visitList)
+
+    for bus in buses[position]:
+        if costs[bus[1]] > cost + bus[0]:
+            costs[bus[1]] = cost + bus[0]
+            heapq.heappush(visitList, (costs[bus[1]], bus[1]))
+
+print(costs[end])
