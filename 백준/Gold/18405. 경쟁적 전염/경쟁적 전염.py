0a255e7203c66ed481e1a8d7e438bf01fdd3314e
WaokE <128684924+WaokE@users.noreply.github.com>
2023-04-27 22:03:11 +0900

    [Gold V] Title: 경쟁적 전염, Time: 132 ms, Memory: 34348 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -1,38 +1,42 @@
-from collections import deque
-import heapq
+
+from heapq import heappush, heappop
 import sys
-input = sys.stdin.readline
 
-N, K = list(map(int, input().split()))
-flask = [list(map(int, input().split()))for _ in range(N)]
-S, X, Y = list(map(int, input().split()))
+n, k = map(int, sys.stdin.readline().split())
 
-def effectVirus(virusNum, x, y):
-    dx = [-1,1,0,0]
-    dy = [0,0,-1,1]
-    for i in range(4):
-        if N-1 < x+dx[i] or x+dx[i] < 0 or N-1 < y+dy[i] or y+dy[i] < 0:
-            continue
-        if flask[x+dx[i]][y+dy[i]] == 0:
-            flask[x+dx[i]][y+dy[i]] = virusNum
+tubes = list(list(map(int, sys.stdin.readline().split())) for _ in range(n))
 
-def checkVirus():
-    result = []
-    for i in range(N):
-        for j in range(N):
-            if flask[i][j] != 0:
-                result.append((flask[i][j], i, j))
-    return result
+s, x, y = map(int, sys.stdin.readline().split())
 
-second = 0
-while second < S:
-    viruses = checkVirus()
-    if len(viruses) == N**2:
-        break
-    heapq.heapify(viruses)
-    while viruses:
-        virusnum, x, y = heapq.heappop(viruses)
-        effectVirus(virusnum, x, y)
-    second += 1
+visit_pq = []
 
-print(flask[X-1][Y-1])
+
+################################################################
+#
+#   우선순위 큐에 [시간, 바이러스 번호] 순으로 시작 지점들을 넣어준다.
+#
+################################################################
+for row in range(n):
+    for col in range(n):
+        if tubes[row][col]:
+            heappush(visit_pq, [0, tubes[row][col], row, col])
+
+
+while visit_pq:
+    now_time, now_virus, now_row, now_col = heappop(visit_pq)
+
+    if now_row > 0 and not tubes[now_row - 1][now_col] and now_time < s:
+        tubes[now_row - 1][now_col] = now_virus
+        heappush(visit_pq, [now_time + 1, now_virus, now_row - 1, now_col])
+    if now_col > 0 and not tubes[now_row][now_col - 1] and now_time < s:
+        tubes[now_row][now_col - 1] = now_virus
+        heappush(visit_pq, [now_time + 1, now_virus, now_row, now_col - 1])
+    if now_row < n - 1 and not tubes[now_row + 1][now_col] and now_time < s:
+        tubes[now_row + 1][now_col] = now_virus
+        heappush(visit_pq, [now_time + 1, now_virus, now_row + 1, now_col])
+    if now_col < n - 1 and not tubes[now_row][now_col + 1] and now_time < s:
+        tubes[now_row][now_col + 1] = now_virus
+        heappush(visit_pq, [now_time + 1, now_virus, now_row, now_col + 1])
+
+
+print(tubes[x - 1][y - 1])
