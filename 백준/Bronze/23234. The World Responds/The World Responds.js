df7924865852204db912950d3d667104e3d14df4
WaokE <128684924+WaokE@users.noreply.github.com>
2024-06-19 17:48:10 +0900

    [Bronze V] Title: The World Responds, Time: 92 ms, Memory: 9320 KB -BaekjoonHub

---
+++ 백준/Bronze/23234. The World Responds/The World Responds.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+// const input = fs.readFileSync("test.txt").toString().trim().split("\n");
+
+function solution(input) {
+    //
+    console.log("The world says hello!")
+}
+
+solution(input);
